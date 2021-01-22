ddd7fba8f9c3472120d59b077ce6b239b09c3840
AnnaGusakN <76492258+AnnaGusakN@users.noreply.github.com>
2021-01-22 15:48:56 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace changeValue
+{
+    //Анна Гусак
+    /* Написать программу обмена значениями двух переменных:
+    a) с использованием третьей переменной */
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter firsst number: ");
+        int x = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter second number: ");
+        int y = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Original x={x}, y={y}");
+
+        int tmp = x;
+        x = y;
+        y = tmp;
+        Console.WriteLine($"Exchanged x={x}, y={y}");
+    }
+
+}
+}
